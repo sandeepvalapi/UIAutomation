616a20a7e640f5a85b2472b400075f1c5d9d2a67
Sandeep Valapi <sandeepvalapi@gmail.com>
2016-10-06 16:41:03 +0530

    Renamed package names

---
+++ discovercucumber/src/test/java/features/RunTest.java
@@ -0,0 +1,10 @@
+package features;
+
+import org.junit.runner.RunWith;
+import cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = { "pretty", "html:target/cucumber" }, features = "features", glue = "com.steps")
+
+public class RunTest {
+}
---
+++ discovercucumber/src/test/java/features/outline.feature
@@ -0,0 +1,6 @@
+Feature: InvalidLogin
+
+  Scenario: 
+    Given I am a user
+    When I enter invalid credential
+    Then Error message is shown
